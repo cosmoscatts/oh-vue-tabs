1cce887d8dffa481f7e7810474ce495f37700752
faitsse <2138889191@qq.com>
2022-06-27 16:19:11 +0800

    chore: init

--- src/composbales.ts
+++ src/composbales.ts
@@ -1,3 +1,32 @@
+import { CssRender } from 'css-render'
+import type { CNode, CNodeChildren, CProperties, CSelector } from 'css-render'
+
+/**
+ * use js to render css
+ */
+export function useCssRender() {
+  const { c } = CssRender()
+
+  let style: CNode
+
+  function cssRender(selector: CSelector, props: CProperties, children: CNodeChildren = []) {
+    style = c(selector, props, children)
+    style.render()
+    style.mount()
+  }
+
+  onUnmounted(() => {
+    style?.unmount()
+  })
+
+  return {
+    cssRender,
+  }
+}
+
+/**
+ * Boolean Ref Factory
+ */
 export function useBoolean(initValue = false) {
   const bool = ref(initValue)
 
---
+++ src/types.ts
@@ -0,0 +1,38 @@
+/** 填充颜色： [默认颜色, 暗黑主题颜色] */
+export type FillColor = [string, string]
+
+/** 混合比例：[默认, 暗黑] */
+export type MixRatio = [number, number]
+
+export interface ButtonTabProps {
+  /** 暗黑模式 */
+  darkMode?: boolean
+  /** 激活状态 */
+  isActive?: boolean
+  /** 主题颜色 */
+  primaryColor?: string
+  /** 边框颜色 */
+  borderColor?: string
+  /** 暗黑模式的边框颜色 */
+  darkBorderColor?: string
+  /** 是否显示关闭图标 */
+  closable?: boolean
+}
+export interface ChromeTabProps {
+  /** 暗黑模式 */
+  darkMode?: boolean
+  /** 激活状态 */
+  isActive?: boolean
+  /** 主题颜色 */
+  primaryColor?: string
+  /** 是否显示关闭图标 */
+  closable?: boolean
+  /** 背景颜色 */
+  bgColor?: FillColor
+  /** 悬浮时的背景颜色 */
+  hoverBgColor?: FillColor
+  /** 激活状态时的混合颜色 */
+  mixColor?: FillColor
+  /** 混合比例(主题颜色的占比) */
+  mixRatio?: MixRatio
+}
