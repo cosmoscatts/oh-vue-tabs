1a560341fd7f34f572e8f0536e5d841bc8120b5c
faitsse <2138889191@qq.com>
2022-06-28 17:14:29 +0800

    chroe: add enum & types

---
+++ src/enum.ts
@@ -0,0 +1,6 @@
+export enum EnumSoftTagBar {
+  primaryColor = '#1890ff',
+  defaultIconColor = '#9ca3af',
+  borderColor = '#e5e7eb',
+  borderDarkColor = '#ffffff3d',
+}
--- src/types.ts
+++ src/types.ts
@@ -1,5 +1,29 @@
-export interface SoftTagBarProps {
+export interface TagProps {
+  isDark?: boolean
+  isActive?: boolean
+  primaryColor?: string
+  closable?: boolean
+}
 
+export interface TagDefaultProps extends TagProps {
+  borderColor?: string
+  borderDarkColor?: string
+}
+
+/** 填充颜色： [默认颜色, 暗黑主题颜色] */
+type FillColor = [string, string]
+/** 混合比例：[默认, 暗黑] */
+type MixRatio = [number, number]
+
+export interface TagChromeProps extends TagProps {
+  bgColor?: FillColor
+  hoverBgColor?: FillColor
+  mixColor?: FillColor
+  mixRatio?: MixRatio
+}
+
+export interface SoftTagBarProps extends TagDefaultProps, TagChromeProps {
+  mode: 'default' | 'chrome'
 }
 
 export interface TagButtonCloseProps {
