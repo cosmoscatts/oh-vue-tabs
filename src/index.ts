b756766cbe0059647612cfb54a736d048e45635d
faitsse <2138889191@qq.com>
2022-06-30 14:04:05 +0800

    feat: update

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,32 @@
 import TagChrome from './components/TagChrome.vue'
 import TagDefault from './components/TagDefault.vue'
 
-// export type { TagDefaultProps, TagChromeProps } from '~/types'
-
 export {
   TagChrome,
   TagDefault,
 }
+
+type FillColor = [string, string]
+type MixRatio = [number, number]
+
+interface TagProps {
+  isDark?: boolean
+  isActive?: boolean
+  primaryColor?: string
+  closable?: boolean
+}
+
+export interface TagDefaultProps extends TagProps {
+  borderColor?: string
+  borderDarkColor?: string
+}
+
+export interface TagChromeProps extends TagProps {
+  bgColor?: FillColor
+  hoverBgColor?: FillColor
+  mixColor?: FillColor
+  mixRatio?: MixRatio
+  isLast?: boolean
+}
+
 
--- src/types.ts
+++ src/types.ts
@@ -1,25 +1,4 @@
-/** 填充颜色： [默认颜色, 暗黑主题颜色] */
 export type FillColor = [string, string]
-/** 混合比例：[默认, 暗黑] */
+
 export type MixRatio = [number, number]
-
-export interface TagProps {
-  isDark?: boolean
-  isActive?: boolean
-  primaryColor?: string
-  closable?: boolean
-}
-
-export interface TagDefaultProps extends TagProps {
-  borderColor?: string
-  borderDarkColor?: string
-}
-
-export interface TagChromeProps extends TagProps {
-  bgColor?: FillColor
-  hoverBgColor?: FillColor
-  mixColor?: FillColor
-  mixRatio?: MixRatio
-  isLast?: boolean
-}
 
